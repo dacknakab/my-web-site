e221f03939eb86ea2734170cdad97e036a189dc7
dackdev <mynameispipat@gmail.com>
2025-03-09 13:26:47 +0700

    edit file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   plugins: [],
 }
